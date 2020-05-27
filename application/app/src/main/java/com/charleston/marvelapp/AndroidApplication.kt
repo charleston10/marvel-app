c0678113eb224d6c6eafda42d92f3b59eedf4a9a
charleton10 <charleston.anjos@gmail.com>
2020-05-27 16:22:35 -0300

    Setup Koin

--- application/app/src/main/java/com/charleston/marvelapp/AndroidApplication.kt
+++ application/app/src/main/java/com/charleston/marvelapp/AndroidApplication.kt
@@ -1,5 +1,19 @@
 package com.charleston.marvelapp
 
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+
 open class AndroidApplication: android.app.Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        startKoin()
+    }
+
+    private fun startKoin(){
+        org.koin.core.context.startKoin {
+            androidContext(applicationContext)
+            androidLogger()
+        }
+    }
 }
