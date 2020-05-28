f1205130ccb54717d78503d459e71b311c007d6f
charleton10 <charleston.anjos@gmail.com>
2020-05-28 10:09:30 -0300

    Add serializable

--- application/data/src/main/java/com/charleston/data/remote/response/ObjectResponse.kt
+++ application/data/src/main/java/com/charleston/data/remote/response/ObjectResponse.kt
@@ -1,8 +1,9 @@
 package com.charleston.data.remote.response
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class ObjectResponse(
     @SerializedName("copyright") val copyright: String,
     @SerializedName("data") val data: DataResponse
-)
+) : Serializable
