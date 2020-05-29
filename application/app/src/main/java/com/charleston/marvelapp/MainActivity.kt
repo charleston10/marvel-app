2b3f7c482f2d7ce7206195e2efa91751f7f4e587
charleton10 <charleston.anjos@gmail.com>
2020-05-29 08:30:07 -0300

    Add LinearSnapHelper

--- application/app/src/main/java/com/charleston/marvelapp/MainActivity.kt
+++ application/app/src/main/java/com/charleston/marvelapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -33,5 +34,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
             adapter = featuredAdapter
         }
+
+        LinearSnapHelper().attachToRecyclerView(list_featured)
     }
 }
