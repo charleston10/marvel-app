c824b764313cf2cc8ab6b222c475a4ee33b36189
charleton10 <charleston.anjos@gmail.com>
2020-05-29 10:16:10 -0300

    adds new image when api doesn't have

--- application/data/src/main/java/com/charleston/data/mappers/CharacterMapper.kt
+++ application/data/src/main/java/com/charleston/data/mappers/CharacterMapper.kt
@@ -14,6 +14,15 @@ class CharacterMapper : IMapper<CharacterResponse, CharacterModel> {
     }
 
     private fun getImageURI(path: String, extension: String): String {
-        return path.plus(".").plus(extension)
+        return if(isNoImage(path)){
+            "https://i.pinimg.com/originals/db/b2/12/dbb2129035f83c491af200bb58e257cc.jpg"
+        }else{
+            path.plus(".").plus(extension)
+        }
+
+    }
+
+    private fun isNoImage(path: String): Boolean {
+        return path.split("/").find { it.contains("image_not_available") }?.isNotEmpty() ?: false
     }
 }
