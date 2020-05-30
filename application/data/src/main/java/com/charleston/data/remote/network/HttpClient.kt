b8192d5bb6b64d0c500e8f7e8c068d846fe48ac3
charleton10 <charleston.anjos@gmail.com>
2020-05-30 13:26:01 -0300

    Enable sniffing http request for debug mode

--- application/data/src/main/java/com/charleston/data/remote/network/HttpClient.kt
+++ application/data/src/main/java/com/charleston/data/remote/network/HttpClient.kt
@@ -1,6 +1,8 @@
 package com.charleston.data.remote.network
 
+import android.annotation.SuppressLint
 import android.app.Application
+import com.charleston.data.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -8,8 +10,13 @@ import okhttp3.Cache
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.security.cert.X509Certificate
 import java.util.*
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class HttpClient(private val application: Application) {
 
@@ -41,6 +48,11 @@ class HttpClient(private val application: Application) {
             .cache(cache)
             .addInterceptor(AuthInterceptor())
 
+        if (BuildConfig.DEBUG) {
+            //enable sniffing http request
+            okHttpBuilder.sslSocketFactory(provideSSLSocketFactory(), provideX509TrustManager())
+        }
+
         okHttpClient = okHttpBuilder.build()
 
         return okHttpClient
@@ -53,5 +65,36 @@ class HttpClient(private val application: Application) {
                 .add(Date::class.java, Rfc3339DateJsonAdapter())
                 .build()
         )
+    }
+
+    private fun provideSSLSocketFactory(): SSLSocketFactory {
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(
+            null,
+            arrayOf<TrustManager>(provideX509TrustManager()),
+            java.security.SecureRandom()
+        )
+        return sslContext.socketFactory
+    }
+
+    @SuppressLint("TrustAllX509TrustManager")
+    private fun provideX509TrustManager(): X509TrustManager {
+        return object : X509TrustManager {
+            override fun checkClientTrusted(
+                chain: Array<out X509Certificate>?,
+                authType: String?
+            ) {
+            }
+
+            override fun checkServerTrusted(
+                chain: Array<out X509Certificate>?,
+                authType: String?
+            ) {
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                return arrayOf()
+            }
+        }
     }
 }
