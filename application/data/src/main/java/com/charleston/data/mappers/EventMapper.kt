b5e093e42ce93564519693c0d2112d06d364642a
charleton10 <charleston.anjos@gmail.com>
2020-05-31 13:03:14 -0300

    Add information date in events

--- application/data/src/main/java/com/charleston/data/mappers/EventMapper.kt
+++ application/data/src/main/java/com/charleston/data/mappers/EventMapper.kt
@@ -3,14 +3,26 @@ package com.charleston.data.mappers
 import com.charleston.data.extensions.getImageUrl
 import com.charleston.data.remote.response.EventResponse
 import com.charleston.domain.model.ItemModel
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EventMapper : IMapper<EventResponse, ItemModel> {
 
     override fun transform(entity: EventResponse): ItemModel {
         return ItemModel(
             name = entity.title,
-            description = entity.description,
+            description = updateDescription(entity.startDate, entity.endDate, entity.description),
             imageUrl = entity.thumbnail.getImageUrl()
         )
+    }
+
+    private fun updateDescription(startDate: Date, endDate: Date, description: String?): String {
+        val dateFormat: DateFormat = SimpleDateFormat("yyyy/MM", Locale("pt", "BR"))
+            .apply {
+                timeZone = TimeZone.getTimeZone("UTC")
+            }
+
+        return "The event occurs ${dateFormat.format(startDate)} to ${dateFormat.format(endDate)}\n$description"
     }
 }
