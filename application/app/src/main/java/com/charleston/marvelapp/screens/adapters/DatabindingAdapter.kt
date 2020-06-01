217cd59de3c538bc5feac2cae81e5e65029b2fa9
charleton10 <charleston.anjos@gmail.com>
2020-06-01 09:22:50 -0300

    Get text in string resource

--- application/app/src/main/java/com/charleston/marvelapp/screens/adapters/DatabindingAdapter.kt
+++ application/app/src/main/java/com/charleston/marvelapp/screens/adapters/DatabindingAdapter.kt
@@ -1,13 +1,13 @@
 package com.charleston.marvelapp.screens.adapters
 
-import android.text.TextWatcher
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.charleston.marvelapp.R
+import java.util.*
 
 class DataBindingAdapter {
 
@@ -34,28 +34,23 @@ class DataBindingAdapter {
         @BindingAdapter(value = ["bindShowing"])
         fun bindShowing(view: TextView, total: Int?) {
             total?.let {
-                view.text = "Showing $it item(s)"
-            }
-        }
-
-        @JvmStatic
-        @BindingAdapter(value = ["bindTextWatcher"])
-        fun bindTextWatcher(view: EditText, textWatcher: TextWatcher?) {
-            textWatcher?.let {
-                view.addTextChangedListener(it)
+                val text = view.context.getString(R.string.label_showing)
+                view.text = String.format(text, it)
             }
         }
 
         @JvmStatic
         @BindingAdapter(value = ["bindTextResult"])
         fun bindTextResult(view: TextView, themeName: String?) {
-            view.text = "List of items theme ${themeName?.toLowerCase()}"
+            val text = view.context.getString(R.string.label_list_theme_selected)
+            view.text = String.format(text, themeName?.toLowerCase(Locale.getDefault()))
         }
 
         @JvmStatic
         @BindingAdapter(value = ["bindTextLoading"])
         fun bindTextLoading(view: TextView, themeName: String?) {
-            view.text = "Loading ${themeName?.toLowerCase()}"
+            val text = view.context.getString(R.string.label_list_loading)
+            view.text = String.format(text, themeName?.toLowerCase(Locale.getDefault()))
         }
     }
 }
