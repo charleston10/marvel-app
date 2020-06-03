899c4215af81efc474730ab902f87cc779f2b5a6
charleton10 <charleston.anjos@gmail.com>
2020-06-03 09:54:22 -0300

    Load modules and config url provider

--- application/app/src/main/java/com/charleston/marvelapp/AndroidApplication.kt
+++ application/app/src/main/java/com/charleston/marvelapp/AndroidApplication.kt
@@ -1,25 +1,28 @@
 package com.charleston.marvelapp
 
 import com.charleston.data.DataModule
+import com.charleston.data.remote.network.UrlProvider
 import com.charleston.domain.DomainModule
 import com.charleston.marvelapp.di.AppModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 open class AndroidApplication : android.app.Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin()
+        startKoin {
+            androidLogger()
+            androidContext(this@AndroidApplication)
+            loadModules()
+        }
     }
 
-    private fun startKoin() {
-        org.koin.core.context.startKoin {
-            androidContext(this@AndroidApplication)
-            AppModule.loadModule()
-            DomainModule.loadModule()
-            DataModule.loadModule()
-            androidLogger()
-        }
+    fun loadModules(urlProvider: UrlProvider? = null) {
+        AppModule.loadModule()
+        DomainModule.loadModule()
+        DataModule.loadModule()
+        DataModule.loadNetworkModule(urlProvider)
     }
 }
