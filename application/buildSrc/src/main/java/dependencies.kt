56195ba3c57ddbbe88db9a4165f997994bead75c
charleton10 <charleston.anjos@gmail.com>
2020-06-03 09:52:25 -0300

    Add dependencies for Test and Log Interceptor

--- application/buildSrc/src/main/java/dependencies.kt
+++ application/buildSrc/src/main/java/dependencies.kt
@@ -12,6 +12,7 @@ internal object Versions {
     const val materialDesign = "1.1.0"
     const val retrofit = "2.7.0"
     const val squareMoshi = "1.9.2"
+    const val koin = "2.0.1"
 }
 
 object BuildPlugins {
@@ -30,11 +31,12 @@ object Dependencies {
     val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
     val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
     val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
-    val koin = "org.koin:koin-android:2.0.1"
+    val koin = "org.koin:koin-android:${Versions.koin}"
     val koinViewModel = "org.koin:koin-android-viewmodel:2.0.1"
     val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
     val retrofitConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
     val retrofitMoshi = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+    val okHttpLogger    = "com.squareup.okhttp3:logging-interceptor:3.8.0"
     val squareMoshi = "com.squareup.moshi:moshi:${Versions.squareMoshi}"
     val squareMoshiKotlin = "com.squareup.moshi:moshi-kotlin:${Versions.squareMoshi}"
     val squareMoshiAdapter = "com.squareup.moshi:moshi-adapters:${Versions.squareMoshi}"
@@ -44,9 +46,22 @@ object Dependencies {
 }
 
 object TestDependencies {
-    val jUnit = "junit:junit:4.12"
+    val androidxRunner = "androidx.test:runner:1.2.0"
     val jUnitExt = "androidx.test.ext:junit:1.1.1"
     val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+    val espressoIntents = "androidx.test.espresso:espresso-intents:3.2.0"
+    val androidxTruth = "androidx.test.ext:truth:1.2.0"
+
+    val jUnit = "junit:junit:4.12"
+    val espressoRunner = "androidx.test:runner:1.0.2"
+    val espressoContrib = "androidx.test.espresso:espresso-contrib:3.2.0"
+    val archCore = "androidx.arch.core:core-testing:2.0.1"
+    val testCore = "androidx.test:core:1.2.0"
+    val koin = "org.koin:koin-test:${Versions.koin}"
+    val mockk = "io.mockk:mockk-android:1.9.3"
+    val roboletric = "org.robolectric:robolectric:4.3"
+    val kakao = "com.agoda.kakao:kakao:2.1.0"
+    val mockWebServer = "com.squareup.okhttp3:mockwebserver:3.8.0"
 }
 
 object AndroidModule {
@@ -74,7 +89,8 @@ object AndroidModule {
         Dependencies.squareMoshi,
         Dependencies.squareMoshiKotlin,
         Dependencies.squareMoshiAdapter,
-        Dependencies.commonsIo
+        Dependencies.commonsIo,
+        Dependencies.okHttpLogger
     )
 
     val domain = listOf(
@@ -87,7 +103,15 @@ object AndroidModule {
     )
 
     val androidTesting = listOf(
+        TestDependencies.androidxRunner,
         TestDependencies.jUnitExt,
-        TestDependencies.espressoCore
+        TestDependencies.espressoCore,
+        TestDependencies.espressoIntents,
+        TestDependencies.androidxTruth,
+        TestDependencies.koin,
+        TestDependencies.mockk,
+        TestDependencies.kakao,
+        TestDependencies.mockWebServer,
+        Dependencies.commonsIo
     )
 }
